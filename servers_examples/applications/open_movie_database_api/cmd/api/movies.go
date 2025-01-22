3556d5121c5fa191ab07e103aac6aaf5c5097b82
codex1400 12 <riddheshjani@proton.me>
2025-01-22 08:59:43 +0000

    second commit

--- servers_examples/applications/open_movie_database_api/cmd/api/movies.go
+++ servers_examples/applications/open_movie_database_api/cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/codex1400/servers_examples/applications/open_movie_database_api/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,5 +17,17 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		Id:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+	err = app.writeJson(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
--- servers_examples/applications/open_movie_database_api/cmd/api/statuscheck.go
+++ servers_examples/applications/open_movie_database_api/cmd/api/statuscheck.go
@@ -1,12 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) statusCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: avaiable")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	data:= map[string]string{
+		"status": "available",
+		"environment": app.config.env,
+		"version": version, 
+	}
+	err:= app.writeJson(w,http.StatusOK,data, nil)
+	if err != nil{
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
